28b22cff4481849e4c4cd4051e19f9f6015eac32
m2hurst-cyber <m2hurst@gmail.com>
2026-04-21 15:25:13 -0400

    Add Microsoft connection storage

---
+++ db/migrate/20260421170000_create_microsoft_connections.rb
@@ -0,0 +1,22 @@
+class CreateMicrosoftConnections < ActiveRecord::Migration[7.2]
+  def change
+    create_table :microsoft_connections do |t|
+      t.references :user, null: false, foreign_key: true, index: { unique: true }
+      t.string :tenant_id, null: false
+      t.string :microsoft_user_id, null: false
+      t.string :email, null: false
+      t.string :display_name
+      t.text :credential_payload
+      t.datetime :credential_expires_at
+      t.text :granted_scopes, default: ""
+      t.datetime :last_synced_at
+      t.datetime :discarded_at
+
+      t.timestamps
+    end
+
+    add_index :microsoft_connections, :microsoft_user_id
+    add_index :microsoft_connections, :tenant_id
+    add_index :microsoft_connections, :discarded_at
+  end
+end
